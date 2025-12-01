19c10cf241b82384fc0c657737ba2c86b875085e
SamDasari123456789 <samuel.dasari@dcmail.ca>
2025-12-01 14:14:10 -0500

    setting up my constants

--- lab4/part1/javaScript.js
+++ lab4/part1/javaScript.js
@@ -24,6 +24,9 @@ function randomValueFromArray(array) {
 // turned into a slug and slithered away
 
 // Partial return random string function
+const characters = ["Gandalf","Froddo","Aragorn"];
+const places = ["The Shire","Mordor","Rivendell"];
+const events = ["pee's his pants","climaxes","starts working out"];
 
 function returnRandomStoryString() {
   // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
