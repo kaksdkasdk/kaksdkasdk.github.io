9724729c9f45005919f5222dc0cc7a98a3389b4f
SamDasari123456789 <samuel.dasari@dcmail.ca>
2025-12-01 15:27:41 -0500

    Update script.js

--- lab4/part3/script.js
+++ lab4/part3/script.js
@@ -92,3 +92,20 @@ function loop() {
 }
 
 loop();
+
+class Ball {
+  // …
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}
