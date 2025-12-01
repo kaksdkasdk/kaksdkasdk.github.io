47180110b68c2726bf1db3a8a9a2c8edcdc699ba
SamDasari123456789 <samuel.dasari@dcmail.ca>
2025-12-01 15:23:13 -0500

    creating the ball object arc and path

--- lab4/part3/script.js
+++ lab4/part3/script.js
@@ -22,3 +22,12 @@ class Ball {
     this.size = size;
   }
 }
+class Ball {
+  // …
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}
