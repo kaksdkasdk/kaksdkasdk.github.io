29acdfe1c9cfc6a47c6ff05f7431024f7b39db03
SamDasari123456789 <samuel.dasari@dcmail.ca>
2025-12-01 16:19:08 -0500

    collision method update

--- lab4/part4/script.js
+++ lab4/part4/script.js
@@ -56,7 +56,7 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (this !== ball) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
